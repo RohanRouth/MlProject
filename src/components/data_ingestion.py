370c1badcf7cfe7e431afa38a4f8e458800121b4
Rohan <rohanrouth@gmail.com>
2023-03-25 03:40:10 -0400

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,7 +8,8 @@ from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
-
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -50,7 +51,16 @@ if __name__=="__main__":
     train_data,test_data=obj.initiate_data_ingestion()     
 
     data_transformation = DataTransformation()
-    data_transformation.Initiate_data_Transformation(train_data,test_data)
+    train_arr,test_arr,_=data_transformation.Initiate_data_Transformation(train_data,test_data)
+
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
+
+    
+
+
+
+
 
 
 
